6764db5926aa78b9040acf43852579a24e346202
Krimera <heatherellenbarker@gmail.com>
2019-10-20 12:11:23 -0500

    udpates

--- static/js/app.js
+++ static/js/app.js
@@ -1,4 +1,34 @@
 // from data.js
 var tableData = data;
 
-// YOUR CODE HERE!
+
+var button = d3.select("#filter-btn");
+
+button.on("click", function () {
+
+    var inputElement = d3.select("#datetime");
+    console.log(inputElement)
+
+    var inputValue = inputElement.property("value");
+    console.log(inputValue)
+    console.log(tableData)
+
+    var filteredData = tableData.filter(data => data.datetime === inputValue);
+    console.log(filteredData)
+
+    var filteredTable = d3.select("tbody");
+    filteredTable.html("");
+
+    function table(data) {
+        data.forEach((element) => {
+            var row = filteredTable.append("tr");
+            Object.values(element).forEach((value) => {
+                var cell = row.append("td");
+                cell.text(value);
+                console.log(value)
+            })
+        });
+    };
+    table(filteredData)
+
+});
